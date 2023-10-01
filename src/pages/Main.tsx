c9a6e17f103d086455bc8a82d20f358bc3168a9f
themms <maximusnewera@mail.ru>
2023-10-02 05:05:30 +0600

    new file:   src/assets/about2.svg 	new file:   src/assets/about3.svg 	new file:   src/assets/aboutfirst.svg 	modified:   src/pages/Main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
+import section1Photo from "../assets/aboutfirst.svg";
+
 export const Main = () => {
   return (
     <>
       <header className="py-5">
         <div className="flex px-5 justify-center">
           <div className="my-5 text-center">
-            <h1 className="font-bold mb-2 text-5xl text-center">
+            <h1 className="font-bold mt-3 text-5xl text-center">
               Полный цикл сервисного обслуживания
             </h1>
           </div>
@@ -12,7 +14,23 @@ export const Main = () => {
         <hr className="mt-10 border-gray-300" />
       </header>
       <section className="py-5">
-
+        <div className="">
+          <div className="">
+            <div className="">
+              <img className=""
+                src={section1Photo} alt="..." />
+            </div>
+            <h2 className="text-4xl font-bold">Автомобильные газозаправочные станции</h2>
+            <p className="text-2xl mb-0">
+              Собственная сеть газозаправочных станций по всему региону, как стационарных, так и мобильных, что позволяет заправлять
+              <br />ваш транспорт и бытовые баллоны СУГ в удобном для вас месте.
+              <br />
+              <br />Осуществляем заправку газгольдеров по заявке в удобное для вас время.
+              <br />
+              <br />Проводим плановую ежемесячную тарировку раздаточных колонок с заменой фильтров вне зависимости от их степени загрязнения.
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
