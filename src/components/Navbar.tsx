9ffd6094351c9c517fbccc51558bf6726d0cf92f
themms <maximusnewera@mail.ru>
2023-10-06 04:22:00 +0600

    add section with info

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import logo from "../assets/agslogo.svg";
 
 export const Navbar = () => {
   return (
-    <nav className="flex p-3 bg-blue-500">
-      <img src={logo} className="w-60" />
+    <nav className="flex p-3 bg-blue-500 px-24">
+      <img src={logo} className="w-52" />
       <ul className="flex space-x-5 my-auto ml-auto mr-20 text-md text-white font-bold">
         <li>
           <a href="/">Главная</a>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import section1Photo from "../assets/aboutfirst.svg";
+import section1Photo from "../assets/about3.svg";
 
 export const Main = () => {
   return (
@@ -6,32 +6,32 @@ export const Main = () => {
       <header className="py-5">
         <div className="flex px-5 justify-center">
           <div className="my-5 text-center">
-            <h1 className="font-bold mt-3 text-5xl text-center">
+            <h1 className="font-extrabold mt-3 text-5xl text-center">
               Полный цикл сервисного обслуживания
             </h1>
           </div>
         </div>
         <hr className="mt-10 border-gray-300" />
       </header>
-      <section className="py-5">
-        <div className="">
-          <div className="">
-            <div className="">
-              <img className=""
-                src={section1Photo} alt="..." />
-            </div>
-            <h2 className="text-4xl font-bold">Автомобильные газозаправочные станции</h2>
-            <p className="text-2xl mb-0">
-              Собственная сеть газозаправочных станций по всему региону, как стационарных, так и мобильных, что позволяет заправлять
-              <br />ваш транспорт и бытовые баллоны СУГ в удобном для вас месте.
-              <br />
-              <br />Осуществляем заправку газгольдеров по заявке в удобное для вас время.
-              <br />
-              <br />Проводим плановую ежемесячную тарировку раздаточных колонок с заменой фильтров вне зависимости от их степени загрязнения.
-            </p>
-          </div>
+      <div className="grid xl:flex xl:flex-row-reverse items-center xl:px-12">
+        <img className="m-auto w-2/3" src={section1Photo} alt="..." />
+        <div className="text-center">
+          <h2 className="text-4xl font-bold">Автомобильные газозаправочные станции</h2>
+          <p className="text-2xl mb-0 text-gray-500">
+            <br />
+            Собственная сеть газозаправочных станций по всему региону, как
+            стационарных, так и мобильных, что позволяет заправлять
+            <br />ваш транспорт и бытовые баллоны СУГ в удобном для вас месте.
+            <br />
+            <br />Осуществляем заправку газгольдеров
+            по заявке в удобное для вас время.
+            <br />
+            <br />Проводим плановую ежемесячную тарировку раздаточных колонок
+            с заменой фильтров вне зависимости от
+            их степени загрязнения.
+          </p>
         </div>
-      </section>
+      </div>
     </>
   );
 };
