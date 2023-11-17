5d5a168cc9679f2644bbbb0c183626398829aad4
themms <maximusnewera@mail.ru>
2023-11-17 07:38:08 +0600

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
 import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { About } from './pages/About';
+import { Main } from './pages/Main';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div>
+      12345
+    </div>
+  );
+};
